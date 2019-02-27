3881752894d4fbfc3ea0ee1de522d38573ee9062
floadyun <floadyun@163.com>
2019-02-27 19:32:39 +0800

    数据更新

--- trade.py
+++ trade.py
@@ -16,7 +16,7 @@ class Symbol:
 symbols = []
 tradeRecords = ['184109.html', '211953.html',  '22769972.htm',  '526398.htm', '63401.htm', '80881309.htm', '220327.html', '223736.html', '2024063.htm',  '196322.html',  '214354.html',
                  '22789423.htm',  '22769972_2.htm', '80885244.htm', '8009113.htm', '8009926.htm']
-tradeRecords = ['526398.htm', '63401.htm']
+tradeRecords = ['63401.htm']
 for tradeRecord in tradeRecords:
     if tradeRecord == '2024063.htm':
         soup = BeautifulSoup(open("file\\trade\\" + tradeRecord, 'r', encoding='utf-8'), 'lxml')
@@ -24,7 +24,7 @@ for tradeRecord in tradeRecords:
         line = 0
         for tr in trs:
             tds = tr.contents
-            if len(tds) > 13:
+            if len(tds) > 25:
                 symbol = tds[1].string
                 if symbol == '\n':
                     break
