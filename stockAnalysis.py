347ca5ab83f7bc6b7231700b560b864ce99d028e
yixiaofei <yixiaofei@spacecg.cn>
2021-10-08 10:05:40 +0800

    股票分析提交

--- stockAnalysis.py
+++ stockAnalysis.py
@@ -19,4 +19,4 @@ for row in csv_reader:
     elif row[0] == '卖出':
         totalSell += float(row[5])
     print(row)
-print('\n交易品种:', len(stocks), '交易量:', totalVolume, '买入:', totalBuy, ';卖出:', totalSell, '盈亏:', round(totalSell - totalBuy))
+print('\n交易品种:', len(stocks), '交易量:', totalVolume/100, '交易次数', csv_reader.line_num/2, '买入:', totalBuy, ';卖出:', totalSell, '盈亏:', round(totalSell - totalBuy))
