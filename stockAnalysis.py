827f6736409b7ed8af149f055228d8b19b849dc7
yixiaofei <yixiaofei@spacecg.cn>
2022-02-18 12:04:18 +0800

    数据更新，修改股票统计方式

--- stockAnalysis.py
+++ stockAnalysis.py
@@ -5,18 +5,24 @@ totalBuy = 0
 totalSell = 0
 totalVolume = 0
 stocks = {}
+trades = {}
 for row in csv_reader:
-    if row[2] in stocks:
-        trade = stocks[row[2]]
-        trade.append(row[2])
+    if row[2] in trades:
+        if row[0] == '买入':
+            trades[row[2]] -= float(row[5])
+        elif row[0] == '卖出':
+            trades[row[2]] += float(row[5])
     else:
-        trade = []
-        trade.append(row[2])
-        stocks[row[2]] = trade
+        if row[0] == '买入':
+            trades[row[2]] = -float(row[5])
+        elif row[0] == '卖出':
+            trades[row[2]] = float(row[5])
     if row[0] == '买入':
         totalBuy += float(row[5])
         totalVolume += int(row[3])
     elif row[0] == '卖出':
         totalSell += float(row[5])
-    print(row)
-print('\n交易品种:', len(stocks), '交易量:', totalVolume/100, '交易次数', csv_reader.line_num/2, '买入:', totalBuy, ';卖出:', totalSell, '盈亏:', round(totalSell - totalBuy))
+
+for symbole,value in trades.items():
+    print('交易品种: ', symbole, '盈亏：', round(value))
+print('\n交易品种:', len(trades), '交易量:', totalVolume/100, '交易次数', csv_reader.line_num/2, '买入:', totalBuy, ';卖出:', totalSell, '总盈亏:', round(totalSell - totalBuy))
