6b9624515b8d36b896ad467fa5023521cd923b45
user <floadyun@163.com>
2022-10-23 20:08:51 +0800

    收手了，整体亏损近十七万刀，130万人民币！！！

--- trade.py
+++ trade.py
@@ -18,7 +18,7 @@ class Symbol:
 
 symbols = []
 tradeRecords = ['184109.html', '211953.html',  '22769972.htm',  '526398.htm', '63401.htm', '80881309.htm', '220327.html', '223736.html', '2024063.htm',  '196322.html',  '214354.html',
-                 '22789423.htm',  '22769972_2.htm', '80885244.htm', '8009113.htm', '8009926.htm', '829679.htm', "831961.htm", "840558.htm", "693009294.htm", "832449.htm", "14211574.htm", "8305986.htm"]
+                 '22789423.htm',  '22769972_2.htm', '80885244.htm', '8009113.htm', '8009926.htm', '829679.htm', "831961.htm", "840558.htm", "693009294.htm", "832449.htm", "14211574.htm",  "14211574_2.htm", "8305986.htm"]
 # tradeRecords = ['829679.htm', "831961.htm", "840558.htm", '832449.htm']
 for tradeRecord in tradeRecords:
     if tradeRecord == '2024063.htm':
